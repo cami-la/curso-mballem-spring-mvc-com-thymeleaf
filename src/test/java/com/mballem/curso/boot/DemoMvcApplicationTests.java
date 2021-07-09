69e8a02f4dad3fb53847b93d75a71f997ba4672b
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-07-09 12:20:16 -0300

    first commit

---
+++ src/test/java/com/mballem/curso/boot/DemoMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mballem.curso.boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
