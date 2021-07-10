a2af43c1fd5b2fefa4f3b4f1af3baf4875022bc5
Camila Cavalcante <camiladsantoscavalcante@gmail.com>
2021-07-10 08:02:08 -0300

    Testando os links no menu

---
+++ src/main/java/com/mballem/curso/boot/web/controller/FuncionarioController.java
@@ -0,0 +1,20 @@
+package com.mballem.curso.boot.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/funcionarios")
+public class FuncionarioController {
+
+    @GetMapping("/cadastrar")
+    public String cadastrar() {
+        return "/funcionario/cadastro";
+    }
+
+    @GetMapping("/listar")
+    public String listar() {
+        return "/funcionario/lista";
+    }
+}
